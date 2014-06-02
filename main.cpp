52a05ccffbb0e2b0ca4282d67d7fecb8abc7d1c9
LucasCP <ice0life@gmail.com>
2014-06-02 08:30:18 -0300

    up1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+using namespace org;
+
+int main(int nargs,char** args[]){
+	int i=0;
+	mCache *cache;
+	mPri *pri;
+	cache=new mCache();
+	pri= new mPri();
+	
+
+	return 0;
+};
