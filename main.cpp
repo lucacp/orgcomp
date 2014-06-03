99e580cbc000428fe5dae5d7b831abb308d4ed45
LucasCP <ice0life@gmail.com>
2014-06-03 11:05:13 -0300

    up2

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
-#include <iostream>
+
+#include "memcache.h"
 
 using namespace std;
 using namespace org;
 
-int main(int nargs,char** args[]){
-	int i=0;
+int main(int nargs,char** args){
+	int i=0,d,o;
 	mCache *cache;
 	mPri *pri;
 	cache=new mCache();
 	pri= new mPri();
-	
+	do{
+		o=0;
+		d=0;
+		cout << "\ndigite opção: ";
+		cin >> i;
+		if(i==1)
+		{
+			cout<<"\ndigite o: ";
+			cin >> o;
+			cout<<"\ndigite d: ";
+			cin >> d;
+			cache->getBloco(o,d,pri);
+			cache->update();
+		}
+		
+	}while(i!=4);
 
 	return 0;
-};
+}
