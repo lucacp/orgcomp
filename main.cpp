59e7b0bbdc8cc4f692c1916b628e47b7f2745e01
LucasCP <ice0life@gmail.com>
2014-06-10 11:40:16 -0300

    main2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "memcache.h"
+void initmemor(int a,int bs,int cs){
+	c=new mCache(a,bs,cs);
+	p=new mPri();
+}
 
 void teste1(){
 	acessaCache(0,true);
@@ -6,15 +10,38 @@ void teste1(){
 	acessaCache(2,false);
 	acessaCache(2,false);
 };
+void teste2(){
+	acessaCache(1,false);
+	acessaCache(1,false);
+	acessaCache(34,true);
+	acessaCache(34,true);
+};
 
-void initmemor(int a,int bs,int cs){
-	c=new mCache(a,bs,cs);
-	p=new mPri();
-}
+
 int main(int nargs,char** args){
 	initmemor(1,32,128);
 	teste1();
 	c->update();
-	teste1();
+	teste2();
 	return 0;
 }
+void org::acessaCache(unsigned long endereco,bool inst){
+		c->count++;
+		int i=0;
+		i=endereco>>c->var;
+		if(c->blo[i%c->var].dado==p->bloc[endereco].dado)
+			cout << "Hit "<<endl;
+		else
+			acessaMemoria(endereco,inst);
+		c->update(endereco);
+		
+			
+	};
+void org::acessaMemoria(unsigned long endereco,bool inst){
+		p->count++;
+		int i=0;
+		i=endereco>>c->var;
+		c->blo[(i%c->var)]=p->bloc[endereco];
+		c->lru[(i%c->var)]=0;
+		cout << "Miss "<<endl;
+	};
